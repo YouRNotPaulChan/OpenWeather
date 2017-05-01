d0fec66a4eab60e375ce899a96dc5b049851cd46
Paul_Chan <15816170703@163.com>
2017-05-01 17:12:42 +0800

    添加后台更新天气功能

--- app/src/main/java/com/example/openweather/WeatherActivity.java
+++ app/src/main/java/com/example/openweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.openweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -52,13 +53,13 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if(Build.VERSION.SDK_INT>=21){
-//            View decorView =getWindow().getDecorView();
-//            decorView.setSystemUiVisibility(
-//             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
-//             View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-//            getWindow().setStatusBarColor(Color.TRANSPARENT);
-//        }
+        if(Build.VERSION.SDK_INT>=21){
+            View decorView =getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
+             View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN);//全屏显示隐藏状态栏
         setContentView(R.layout.activity_weather);
@@ -117,12 +118,6 @@ public class WeatherActivity extends AppCompatActivity {
         String requestBingPic="http://guolin.tech/api/bing_pic";
         HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-
-            }
-
-            @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String bingPic=response.body().string();
                 SharedPreferences.Editor editor=PreferenceManager.
@@ -136,6 +131,12 @@ public class WeatherActivity extends AppCompatActivity {
 
                     }
                 });
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+
             }
         });
     }
@@ -216,5 +217,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
